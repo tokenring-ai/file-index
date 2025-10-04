a349cd6082655d5d89709a181c9678422d9b96fd
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-04 12:50:36 -0700

    Implemented auto-configuration, that allows the package to automatically install into TokenRing apps

--- index.ts
+++ index.ts
@@ -1,16 +1,41 @@
-import {TokenRingPackage} from "@tokenring-ai/agent";
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
+import {z} from "zod";
 import * as chatCommands from "./chatCommands.ts";
+import EphemeralFileIndexProvider from "./EphemeralFileIndexProvider.ts";
+import FileIndexService from "./FileIndexService.ts";
 import packageJSON from './package.json' with {type: 'json'};
 import * as tools from "./tools.ts";
 
-export {default as FileIndexService} from "./FileIndexService.ts";
-export {default as FileIndexProvider} from "./FileIndexProvider.ts";
-export {default as EphemeralFileIndexProvider} from "./EphemeralFileIndexProvider.ts";
+export const FileIndexConfigSchema = z.object({
+  providers: z.record(z.string(), z.any())
+}).optional();
 
 export const packageInfo: TokenRingPackage = {
   name: packageJSON.name,
   version: packageJSON.version,
   description: packageJSON.description,
-  chatCommands,
-  tools
+  install(agentTeam: AgentTeam) {
+    const config = agentTeam.getConfigSlice('fileIndex', FileIndexConfigSchema);
+    if (config) {
+      agentTeam.addTools(packageInfo, tools);
+      agentTeam.addChatCommands(chatCommands);
+      const fileIndexService = new FileIndexService();
+      agentTeam.addServices(fileIndexService);
+
+      if (config.providers) {
+        for (const name in config.providers) {
+          const fileIndexConfig = config.providers[name];
+          switch (fileIndexConfig.type) {
+            case "ephemeral":
+              fileIndexService.registerFileIndexProvider(name, new EphemeralFileIndexProvider());
+              break;
+          }
+        }
+      }
+    }
+  }
 };
+
+export {default as FileIndexService} from "./FileIndexService.ts";
+export {default as FileIndexProvider} from "./FileIndexProvider.ts";
+export {default as EphemeralFileIndexProvider} from "./EphemeralFileIndexProvider.ts";
