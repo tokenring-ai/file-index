fa397da58ef4bf602ae5ac8079aead732a4b12c8
Mark Dierolf <mdierolf@cloudquote.io>
2026-04-11 22:53:20 -0700

    Remove unused dependencies, code cleanup, lint

--- util/sha256.ts
+++
@@ -1,8 +0,0 @@
-import {createHash} from "node:crypto";
-
-/**
- * Calculates SHA256 hash of the input text
- */
-export function sha256(text: string): string {
-  return createHash("sha256").update(text, "utf8").digest("hex");
-}
